f2e153a22c8f626f303f55933e622194fab89f90
Protesilaos Stavrou <info@protesilaos.com>
2022-09-23 13:33:44 +0300

    Bump version to rebuild package for commit c25a06f

--- notmuch-indicator.el
+++ notmuch-indicator.el
@@ -6,7 +6,7 @@
 ;; Maintainer: notmuch-indicator Development <~protesilaos/notmuch-indicator@lists.sr.ht>
 ;; URL: https://git.sr.ht/~protesilaos/notmuch-indicator
 ;; Mailing-List: https://lists.sr.ht/~protesilaos/notmuch-indicator
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, mail
 
