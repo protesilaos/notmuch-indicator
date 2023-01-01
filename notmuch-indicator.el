edf888dd163ddbe69a9136fd88969819c9544813
Protesilaos Stavrou <info@protesilaos.com>
2023-01-01 17:33:32 +0200

    Update copyright years

--- notmuch-indicator.el
+++ notmuch-indicator.el
@@ -1,6 +1,6 @@
 ;;; notmuch-indicator.el --- Display mode line indicator with notmuch-count(1) output -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022  Free Software Foundation, Inc.
+;; Copyright (C) 2022-2023  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: notmuch-indicator Development <~protesilaos/notmuch-indicator@lists.sr.ht>
