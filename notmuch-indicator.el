49b04988de62e797b0d02b05fc0d447558c26138
Protesilaos Stavrou <info@protesilaos.com>
2023-10-14 10:41:15 +0300

    Change mailing list to my "general issues"
    This is a small package. We do not need a separate list for it.

--- notmuch-indicator.el
+++ notmuch-indicator.el
@@ -3,9 +3,9 @@
 ;; Copyright (C) 2022-2023  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
-;; Maintainer: notmuch-indicator Development <~protesilaos/notmuch-indicator@lists.sr.ht>
+;; Maintainer: notmuch-indicator Development <~protesilaos/general-issues@lists.sr.ht>
 ;; URL: https://git.sr.ht/~protesilaos/notmuch-indicator
-;; Mailing-List: https://lists.sr.ht/~protesilaos/notmuch-indicator
+;; Mailing-List: https://lists.sr.ht/~protesilaos/general-issues
 ;; Version: 1.0.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, mail
