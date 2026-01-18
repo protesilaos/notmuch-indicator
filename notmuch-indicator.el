d25ece77e8ead14e6f37a2d8c05f19cb60362164
Protesilaos Stavrou <info@protesilaos.com>
2026-01-18 08:38:59 +0200

    Update notmuch-indicator to version 1.3.0

--- notmuch-indicator.el
+++ notmuch-indicator.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/notmuch-indicator
-;; Version: 1.2.0
+;; Version: 1.3.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, mail
 
